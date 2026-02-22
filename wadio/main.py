d83203f2bcede0a4f053dd32a6cc1bfb69b236b7
bestewoog <bestewoog@example.com>
2026-02-22 18:34:02 +0900

    chore: enable hot reload for development

--- wadio/main.py
+++ wadio/main.py
@@ -29,4 +29,4 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=21302)
+    uvicorn.run("wadio.main:app", host="0.0.0.0", port=21302, reload=True)
